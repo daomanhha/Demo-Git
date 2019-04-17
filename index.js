9f6b7df684fe3a9543ee40044c7aa80b8933c311
unknown <daoha9811@gmail.com>
2019-04-17 23:40:45 +0700

    Index

---
+++ index.js
@@ -0,0 +1,6 @@
+var ys = require('./Yasou.js');
+var ak = require('./Akali.js');
+var Yasou = new ys();
+var Akali = new ak();
+console.log(Yasou.name, Yasou.sound);
+console.log(Akali.name, Akali.sound);
